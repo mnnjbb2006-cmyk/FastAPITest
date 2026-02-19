125fc329ccc0d9cd01741c8283655dbaee4a6c95
Mani Jabbari <mnnjbb.2006@gmail.com>
2026-02-19 17:15:29 +0330

    upload and change cover added

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, UploadFile
 from typing import List, Annotated, Optional, Dict
 from pydantic import BaseModel, Field
 
@@ -12,6 +12,7 @@ class BookBase(BaseModel):
 
 class Book(BookBase):
      book_id: int
+     coverPath: Optional[str] = None
 
 
 class BookUpdate(BaseModel):
@@ -212,4 +213,15 @@ def delete_book(book_id: int) -> dict:
                poped_ids.append(book_id)
                books.pop(i)
                return {"message": f"delete completed"}
+     raise HTTPException(status_code=404, detail="Book not found")
+
+@api.put("/{book_id}/cover")
+def update_cover(book_id: int, cover_image: UploadFile) -> dict:
+     for book in books:
+          if book.book_id == book_id:
+               coverPath = f"images/{book_id}"
+               with open(coverPath, "wb") as f:
+                    f.write(cover_image.file.read())
+               book.coverPath = coverPath
+               return {"message": "Cover image added/updated"}
      raise HTTPException(status_code=404, detail="Book not found")
